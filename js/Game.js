cbc69ed663f17ae04fc271e411532d6c3da7db07
Kaya Jackson <kayajackson27@gmail.com>
2020-10-29 19:51:19 -0400

    first commit

---
+++ js/Game.js
@@ -0,0 +1,21 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+ class Game {
+     constructor() {
+         this.missed;
+         this.phrases = [
+            new Phrase = 'big smile',
+            new Phrase = 'cheesy grin',
+            new Phrase = 'happy face',
+            new Phrase = 'silly willy',
+            new Phrase = 'goofy goober',
+            new Phrase = 'good vibes',
+            new Phrase = 'positive energy',
+            new Phrase = 'namaste'
+
+         ];
+         this.activePhrase;
+     }
+ }
---
+++ js/app.js
@@ -0,0 +1,13 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * app.js */
+
+// const phrase = new Phrase('Life is like a box of chocolate');
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+const game = new Game();
+
+game.phrases.forEach( (phrase, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrase.phrases}`);
+    
+});
