de334049e344ad4a07488f7b5e1cbad5c941205e
Kaya Jackson <kayajackson27@gmail.com>
2020-11-13 16:54:56 -0500

    step 10

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
-
-// const phrase = new Phrase('Life is like a box of chocolate');
-// console.log(`Phrase - phrase: ${phrase.phrase}`);
-
-// const game = new Game();
-
-// game.phrases.forEach((phrase, index) => {
-//     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-    
-// });
-
-// const randomPhrase = game.getRandomPhrase();
-// const phrase = new Phrase(randomPhrase.phrase);
-// phrase.addPhraseToDisplay();
-
-// game.startGame();
-// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
 let game;
 const startButton = document.getElementById('btn__reset');
@@ -27,3 +7,18 @@ startButton.addEventListener('click', (e) => {
     game.startGame();
 });
 
+const qwerty = document.getElementById('qwerty');
+qwerty.addEventListener('click', (e) => {
+    if (e.target.className === 'key') {
+        game.handleInteraction(e.target);
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    const keyboard = document.querySelectorAll('.key');
+    for (let i = 0; i < keyborad.length; i++ ) {
+        if (keyboard[i].textContent === e.key) {
+            game.handleInteraction(keyboard[i]);
+        }
+    }
+});
